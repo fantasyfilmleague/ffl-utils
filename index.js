6986b6c974fd5bea5f356b113fe8c10eae33f424
Colin Teal <the30yearswar@hotmail.com>
2014-10-27 20:40:17 -0700

    Load from the correct directory

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 'use strict';
 
+var path = require('path');
 var requireByDir = require('./lib/requireByDir');
-requireByDir(__filename, __dirname, exports);
+
+requireByDir(__filename, path.join(__dirname, './lib'), exports);
